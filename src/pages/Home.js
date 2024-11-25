0511e2bf2b543d4cfc83b38e54412de063c04ae0
Douglas Dias Leal <douglas_san@hotmail.com>
2024-11-26 06:54:05 +0900

    feat: create CLI project

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>React Native</Text>
+      <Text>Ignite</Text>
+    </View>
+  )
+}
