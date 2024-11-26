e5fd2f9d3b4eae50bd09475a7b2d60ad7cd35d4d
Douglas Dias Leal <douglas_san@hotmail.com>
2024-11-27 07:12:20 +0900

    feat: add button and state variables

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { 
   Text, 
-  SafeAreaView, 
+  View, 
   StyleSheet, 
   TextInput, 
   TouchableOpacity,
@@ -8,25 +9,47 @@ import {
 } from "react-native";
 
 export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Welcome, Douglas</Text>
 
       <TextInput
         style={styles.input}
         placeholder="New skill"
         placeholderTextColor="#555"
+        onChangeText={setNewSkill}
       />
 
       <TouchableOpacity 
         style={styles.button}
         activeOpacity={0.7}
+        onPress={handleAddNewSkill}
       >
         <Text style={styles.buttonText}>Add</Text>
       </TouchableOpacity>
 
-      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
-    </SafeAreaView>
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+        My Skills
+      </Text>
+
+      {
+        mySkills.map(skill => (
+          <TouchableOpacity key={skill} style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+              {skill}
+            </Text>
+          </TouchableOpacity>
+        ))
+      }
+
+    </View>
   )
 }
 
@@ -34,9 +57,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121015',
-    paddingHorizontal: 30,
-    paddingVertical: 70,
-
+    paddingHorizontal: 20,
+    paddingVertical: 70
   },
   title: {
     color: '#fff',
@@ -45,6 +67,7 @@ const styles = StyleSheet.create({
   },
   input: {
     backgroundColor: '#1f1e25',
+    color: '#FFF',
     fontSize: 18,
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
@@ -61,5 +84,17 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 17,
     fontWeight: 'bold',
-  }
+  },
+  buttonSkill: {
+    backgroundColor: '#1f1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  textSkill: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 })
